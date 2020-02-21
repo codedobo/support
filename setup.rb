77d1cd33de690503d0649435783a8b05d80c08a2
CodeDoctorDE <wusel905@gmail.com>
2020-02-21 14:14:54 +0100

    fix bugs

--- setup.rb
+++ setup.rb
@@ -10,20 +10,20 @@ class SupportModule
       `ROLE` bigint(255) NOT NULL,
       PRIMARY KEY  (`SERVERID`)
     );")
-    send_message "\u001b[32mSuccessfully set up uno module!"
+    send_message "\u001b[32mSuccessfully set up support module!"
   end
 
   def join(server, _already)
-    send_message "\u001b[96mSet up uno module for #{server.id}..."
+    send_message "\u001b[96mSet up support module for #{server.id}..."
     id = server.id
     theme = 'default'
-    @client.query("INSERT INTO `uno` VALUES (#{id},'#{theme}',NULL) ON DUPLICATE KEY UPDATE THEME='#{theme}';")
-    send_message "\u001b[32mSuccessfully set up uno module for #{server.id}!"
+    @client.query("INSERT INTO `support` VALUES (#{id},'#{theme}',NULL) ON DUPLICATE KEY UPDATE THEME='#{theme}';")
+    send_message "\u001b[32mSuccessfully set up support module for #{server.id}!"
   end
 
   def reload(server)
-    send_message "\u001b[96mReloading uno module for server #{server.name}(#{server.id})..."
+    send_message "\u001b[96mReloading support module for server #{server.name}(#{server.id})..."
     match_making
-    send_message "\u001b[32mSuccefully reloaded the uno module for the server!"
+    send_message "\u001b[32mSuccefully reloaded the support module for the server!"
   end
 end
