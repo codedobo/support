f8fbb2c29d6392c595d21ed8a555cc750039a3aa
CodeDoctorDE <wusel905@gmail.com>
2020-02-21 15:08:19 +0100

    fix localjumperror

--- supportchat.rb
+++ supportchat.rb
@@ -4,14 +4,16 @@ require_relative './index.rb'
 class SupportModule
   def support
     @app_class.module_manager.bot.discord.message do |event|
-        results = @client.query("SELECT * FROM `support` WHERE SERVERID='#{event.server.id}';")
-        return if event.author.bot_account?
-        return if results.size != 1
-        return if results.first['CHANNEL'] != event.channel.id
-        event.server.online_members(include_idle: false, include_bots: false).each do |member|
-            return if member.role? results.first['ROLE']
-        end
+      results = @client.query("SELECT * FROM `support` WHERE SERVERID='#{event.server.id}';")
+      return if event.author.bot_account?
+      return if results.size != 1
+      return if results.first['CHANNEL'] != event.channel.id
+
+      if event.server.online_members(include_idle: false, include_bots: false).select do |member|
+        member.role? results.first['ROLE']
+      end.length <= 0
         event.send_message @language.get_json(event.server.id)['event']['notonline']
+      end
     end
   end
 end
