61c69e8542576e79d0d1de744b4981ad38347f61
CodeDoctorDE <wusel905@gmail.com>
2020-02-21 15:04:37 +0100

    fix bot

--- supportchat.rb
+++ supportchat.rb
@@ -5,6 +5,7 @@ class SupportModule
   def support
     @app_class.module_manager.bot.discord.message do |event|
         results = @client.query("SELECT * FROM `support` WHERE SERVERID='#{event.server.id}';")
+        return if event.author.bot_account?
         return if results.size != 1
         return if results.first['CHANNEL'] != event.channel.id
         event.server.online_members(include_idle: false, include_bots: false).each do |member|
