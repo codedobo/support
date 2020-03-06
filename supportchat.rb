1fcf911460ada12150bcf0e9a3fa6ddfda43ed67
CodeDoctorDE <wusel905@gmail.com>
2020-03-06 18:27:31 +0100

    fix bug

--- supportchat.rb
+++ supportchat.rb
@@ -19,7 +19,7 @@ class SupportModule
     @app_class.module_manager.bot.discord.presence do |event|
       results = @client.query("SELECT * FROM `support` WHERE SERVERID='#{event.server.id}';")
       if !event.user.bot_account? && event.status == :online && results.size == 1 && event.user.on(event.server).role?(results.first['ROLE'])
-        event.bot.send_embed do |embed|
+        event.channel.send_embed do |embed|
           embed.title = @language.get_json(event.server.id)['event']['online']['title']
           embed << format(@language.get_json(event.server.id)['event']['online']['body'], event.user.name)
         end
