9a34892fd0b2d9c759398044e5117eedb3b85f69
CodeDoctorDE <proficodergames@gmail.com>
2020-03-07 18:04:55 +0100

    longer support

--- supportchat.rb
+++ supportchat.rb
@@ -32,7 +32,7 @@ class SupportModule
         end
         notification_cooldown << event.user.id
         Thread.new do
-          sleep(60 * 10)
+          sleep(60 * 30)
           notification_cooldown.delete event.user.id
         end
       end
